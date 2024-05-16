b588984eba6ddeada9475f18d55ea4f5e2946b43
minikozort <gokselgokkocabas@gmail.com>
2024-05-16 10:51:46 -0400

    edited script.js to make the add task button to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 
-
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     if (nextId === null) {
@@ -21,10 +20,13 @@ function generateTaskId() {
 }
 
 
+
+
 // Todo: create a function to create a task card
 function createTaskCard(task) {
     const taskCard = $('<div>')
         .addClass('card project-card draggable my-3')
+        .attr('data-task-id', task.id);
     const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
     const cardBody = $('<div>').addClass('card-body');
     const cardDescription = $('<p>').addClass('card-text').text(task.description);
@@ -32,6 +34,7 @@ function createTaskCard(task) {
     const cardDeleteBtn = $('<button>')
         .addClass('btn btn-danger delete')
         .text('Delete')
+        .attr('data-task-id', task.id);
     cardDeleteBtn.on('click', handleDeleteTask);
 
 
@@ -56,27 +59,32 @@ function createTaskCard(task) {
 
     // ? Return the card so it can be appended to the correct lane.
     return taskCard;
-
-
-
 }
 
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-    var taskList = [taskTitleInputEl, taskDueDateInputEl, taskDescriptionInputEl];
+//Todo: create a function to render the task list and make cards draggable
+function renderTaskList(taskList) {
 
-    $.each(taskList, function (_index, taskCard) {
-        $('#to-do').append(taskCard);
-    });
+    if (!taskList) {
+        taskList = []
+    }
+
+    // for (let task of taskList) {
+    //     if (taskList.status === 'to-do') {
+    //       todoList.append(createProjectCard(project));
+    //     } else if (taskList.status === 'in-progress') {
+    //       inProgressList.append(createProjectCard(project));
+    //     } else if (taskList.status === 'done') {
+    //       doneList.append(createProjectCard(project));
+    //     }
+    //   }
+
+    return taskList
 };
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
 
     event.preventDefault();
-
-
-    localStorage.setItem('tasks', JSON.stringify(taskList));
 
     // ? Read user input from the form
     const taskTitleInputEl = $('#title-name-input').val();
@@ -87,18 +95,27 @@ function handleAddTask(event) {
     const newTask = {
         // ? Here we use a Web API called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
         id: generateTaskId(),
-        name: taskTitle,
-        description: taskDescription,
-        dueDate: taskDueDate,
-
+        title: taskTitleInputEl,
+        description: taskDueDateInputEl,
+        dueDate: taskDescriptionInputEl
     };
 
+    const taskList = localStorage.getItem('tasks');
 
-    // ? Pull the projects from localStorage and push the new project to the array
-    let tasks = taskList
-    tasks.push(newTask);
+    taskList.push(newTask);
 
-    newTask.append($('#to-do'));
+    console.log(taskList);
+
+    localStorage.setItem('tasks', JSON.stringify(newTask));
+
+
+    $('#to-do').append(newTask)
+
+    $('#addTaskBtn').on('submit', createTaskCard(newTask));
+
+    renderTaskList();
+
+    
     // ? Clear the form inputs
     taskTitleInputEl.val('');
     taskDescriptionInputEl.val('');
@@ -128,12 +145,15 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+    console.log(taskList);
+
+    renderTaskList();
+
+
     $(function () {
         $("#taskDueDate").datepicker();
     });
 
-
-    $('#taskForm').on('submit', createTaskCard);
 });
 
 
